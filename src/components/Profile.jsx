7d6d64c225409a3b436bc17bfeff0a4cd5904dcc
martos-art <martos.artm@gmail.com>
2020-02-05 13:54:49 +0200

    init commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://media.proglib.io/wp-uploads/2017/05/reactjs-1024x512.png' />
+      </div>
+      <div>
+        ava + description
+        </div>
+      <div>
+        my posts
+          <div>
+          New post
+          </div>
+        <div>post 1</div>
+        <div>post 2</div>
+        <div>pos  3</div>
+        <div>post 4</div>
+        <div>post 5</div>
+      </div>
+
+    </div>
+  )
+};
+
+export default Profile;
