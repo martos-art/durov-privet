51c8c488e7dfe006db86661abe642292e512f801
martos-art <matros.artm@gmail.com>
2020-12-03 15:32:23 +0300

    users api, axios, response, json

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
 import style from './Users.module.css'
-
+import * as axios from "axios";
+import avatar from  '../../accets/images/avatar.png'
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                avatarUrl: 'https://ic.pics.livejournal.com/imit_omsu/66140468/454967/454967_800.jpg',
-                followed: true,
-                fullName: 'Anna',
-                status: 'Uchis davay!!!!!!!!!!!!!!!!!',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                avatarUrl: 'https://sun9-45.userapi.com/c847020/v847020977/1df81/fz46D74JsRQ.jpg?ava=1',
-                followed: false,
-                fullName: 'MAMA',
-                status: 'Kogda uje na rabotu?',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 3,
-                avatarUrl: 'https://cs5.pikabu.ru/images/big_size_comm/2015-04_1/1428229475636.jpg',
-                followed: false,
-                fullName: 'PAPA',
-                status: 'Mojet dalnoboishikom stanesh?',
-                location: {city: 'Minsk', country: 'Belarus'}
-            }
-        ])
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            debugger;
+            props.setUsers(response.data.items)
+        });
     }
 
     return (
@@ -38,7 +18,7 @@ const Users = (props) => {
                 props.users.map((u) => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.avatarUrl} className={style.avatar}/>
+                        <img src={u.photos.small != null ? u.photos.small : avatar } className={style.avatar}/>
                     </div>
                     <div>
                         {u.followed
@@ -49,12 +29,12 @@ const Users = (props) => {
 
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.city}</div>
-                            <div>{u.location.country}</div>
+                            {/*<div>{u.location.city}</div>*/}
+                            {/*<div>{u.location.country}</div>*/}
                         </span>
                     </span>
 
